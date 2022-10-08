4a515a71f8a03b4d71a77d7f785d80777b788449
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-10-08 19:54:45 +0300

    fix contacts css

--- src/components/Kontaktai.jsx
+++ src/components/Kontaktai.jsx
@@ -6,19 +6,21 @@ import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 const Kontaktai = () => {
   return (
     <div className='contacts container'> 
-        <div className='contacts-inner' style={{backgroundImage: `url(${images[2].img})`}}>
-            <div className='contact-section'>
-                <p className='title'>Sekite naujienas</p>
-                <p><a className="social" href="https://www.facebook.com/meskinumedus" target="blank"><FontAwesomeIcon icon={faFacebook} /> Facebook</a></p>
-                <p><a className="social" href="https://www.instagram.com/meskinumedus/" target="blank"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></p>
-            </div>
-            <div className='contact-section'>
-                <p className='title'>Susisiekite su mumis</p>
-                <p><a className="social" href="tel:+370 606 00 222"><FontAwesomeIcon icon={faPhone} /> Paskambinkite dabar</a></p>
-                <p>
-                    <a className="social" href="mailto:meskinumedus@gmail.com?subject=Medus&body=Sveiki, noriu medaus"><FontAwesomeIcon icon={faEnvelope} /> Parašykite el. laišką </a>
-                </p>
-            </div>      
+        <div className='contacts-inner' style={{backgroundImage: `url(${images[1].img})`}}>
+            <div className='contact-wrapper'>
+                <div className='contact-section'>
+                    <p className='title'>Sekite naujienas</p>
+                    <p><a className="social" href="https://www.facebook.com/meskinumedus" target="blank"><FontAwesomeIcon icon={faFacebook} /> Facebook</a></p>
+                    <p><a className="social" href="https://www.instagram.com/meskinumedus/" target="blank"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></p>
+                </div>
+                <div className='contact-section'>
+                    <p className='title'>Susisiekite su mumis</p>
+                    <p><a className="social" href="tel:+370 606 00 222"><FontAwesomeIcon icon={faPhone} /> Paskambinkite dabar</a></p>
+                    <p>
+                        <a className="social" href="mailto:meskinumedus@gmail.com?subject=Medus&body=Sveiki, noriu medaus"><FontAwesomeIcon icon={faEnvelope} /> Parašykite el. laišką </a>
+                    </p>
+                </div>      
+            </div>            
         </div>
     </div>
   )
