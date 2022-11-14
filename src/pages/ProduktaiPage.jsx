44a36a44c85fce87f845bb7c7bcf1ea995bf7387
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-11-14 19:53:13 +0200

    Add h1

--- src/pages/ProduktaiPage.jsx
+++ src/pages/ProduktaiPage.jsx
@@ -3,7 +3,7 @@ import Produktai from "../components/Produktai"
 const ProduktaiPage = () => {
   return (
     <>
-    
+    <h1 className="product-h1">Produktai</h1>
     <div className='produktai-container'>
       <Produktai />
     </div>
