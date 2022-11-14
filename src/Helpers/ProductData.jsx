5185811b40c2ba759239b2b071efdaaad1375e0b
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-11-14 18:54:54 +0200

    Add more products

--- src/Helpers/ProductData.jsx
+++ src/Helpers/ProductData.jsx
@@ -3,9 +3,122 @@ import MedusDovanele from "../images/medus_dovanele.jpg";
 import Muilas from "../images/muilas.jpg";
 import MedusBiciuDuonele from "../images/biciu_duoneles_medus.jpg";
 import SokoladinisMedus from "../images/sokoladinis_medus.jpg";
+import Arkliukas from "../images/Arkliukas.jpg";
+import Burbulas from "../images/Burbulas.jpg";
+import Eglute from "../images/Eglute.jpg";
+import Elniukas from "../images/Elniukas.jpg";
+import Snaige from "../images/Snaige.jpg";
+import Varpelis from "../images/Varpelis.jpg";
+import RinkAngelas from "../images/Rink_Angelas.jpg";
+import RinkArkliukas from "../images/Rink_Arkliukas.jpg";
+import RinkDovanele from "../images/Rink_Dovanele.jpg";
+import RinkKiskioMetai from "../images/Rink_Kiskio_metai.jpg";
+import RinkZaisliukas from "../images/Rink_Zaisliukai.jpg";
+
 
 
 export const products = [
+    {
+        title: "Arkliukas",
+        subtitle: "Rankų darbo vaško žaisliukas eglutei.",
+        img: Arkliukas,
+        price: "Kaina 1.5 Eur",
+        alt: ""
+    },
+    {
+        title: "Burbulas",
+        subtitle: "Rankų darbo vaško žaisliukas eglutei.",
+        img: Burbulas,
+        price: "Kaina 1.5 Eur",
+        alt: ""
+    },
+    {
+        title: "Eglutė",
+        subtitle: "Rankų darbo vaško žaisliukas eglutei.",
+        img: Eglute,
+        price: "Kaina 1.5 Eur",
+        alt: ""
+    },
+    {
+        title: "Elniukas",
+        subtitle: "Rankų darbo vaško žaisliukas eglutei.",
+        img: Elniukas,
+        price: "Kaina 1.5 Eur",
+        alt: ""
+    },
+    {
+        title: "Snaigė",
+        subtitle: "Rankų darbo vaško žaisliukas eglutei.",
+        img: Snaige,
+        price: "Kaina 1.5 Eur",
+        alt: ""
+    },
+    {
+        title: "Varpelis",
+        subtitle: "Rankų darbo vaško žaisliukas eglutei.",
+        img: Varpelis,
+        price: "Kaina 1.5 Eur",
+        alt: ""
+    },
+    {
+        title: `Rinkinukas „Angelas"`,
+        subtitle: `Rinkinuko „Angelas“ sudėtis:
+        medus su bičių duonele (230 g);
+        muilas su medumi;
+        žiedadulkės (40 g);
+        vaško žvakė „Angelas“.`,
+        img: RinkAngelas,
+        price: "Kaina 14 Eur",
+        alt: ""
+    },
+    {
+        title: `Rinkinukas „Vaško arkliukas"`,
+        subtitle: `Rinkinuko „Vaško arkliukas“ sudėtis:
+        medus su bičių duonele (230 g);
+        žiedadulkės (40 g);
+        natūralus lūpų balzamas;
+        vaško žaisliukas „Arkliukas“.`,
+        img: RinkArkliukas,
+        price: "Kaina 14 Eur",
+        alt: ""
+    },
+    {
+        title: `Rinkinukas „Dovanėlė"`,
+        subtitle: `Rinkinuko „Dovanėlė“ sudėtis:
+        medus su bičių duonele (230 g);
+        medus (140 g);
+        žiedadulkės (40 g);
+        vaško žvakė „Dovanėlė“.`,
+        img: RinkDovanele,
+        price: "Kaina 14 Eur",
+        alt: ""
+    },
+    {
+        title: `Rinkinukas „Kiškio metai 2023"`,
+        subtitle: `Rinkinuko „Kiškio metai 2023“ sudėtis:
+        medus (140 g);
+        muilas su medumi;
+        bičių duonelė (40 g);
+        žiedadulkės (40 g);
+        medaus mentelė;
+        vaško žvakė „Kiškutis“.`,
+        img: RinkKiskioMetai,
+        price: "Kaina 14 Eur",
+        alt: ""
+    },
+    {
+        title: `Rinkinukas „Vaško žaisliukai"`,
+        subtitle: `Rinkinuko „Vaško žaisliukai“ sudėtis:
+        vaško žaisliukas „Arkliukas“;
+        vaško žaisliukas „Varpelis“;
+        vaško žaisliukas „Burbulas“;
+        vaško žaisliukas „Kalėdų elnias“;
+        vaško žaisliukas „Eglutė“;
+        vaško žaisliukas „Snaigė“.`,
+        img: RinkZaisliukas,
+        price: "Kaina 14 Eur",
+        alt: ""
+    },
     {
         title: "Natūralus lūpų balzamas",
         subtitle: "100 % natūralus lūpų balzamas. Sudėtis: medus, vaškas, migdolų aliejus, bičių pikis. Tai puikus pasirinkimas kasdienei lūpų odos priežiūrai tiek šaltuoju sezonu, tiek vasaros metu. Reguliariai naudojant lūpos saugomos nuo sausėjimo bei skilinėjimo. Grynas svoris - 5 g.",
@@ -19,14 +132,7 @@ export const products = [
         img: MedusDovanele,
         price: "Kaina 3 Eur",
         alt: ""
-    },
-    {
-        title: "Muilas",
-        subtitle: "Šis muilas subalansuotas odos puoselėjimui, nes jo sudėtyje yra natūralaus bičių medaus bei šaltalankių aliejaus, kurie drėkina ir minkština odą. Sudėtis: natūralus bičių medus, šaltalankių aliejus, kvapusis aliejus, ožkos pienas, aukštos kokybės muilo bazė. Svoris - 50 g.",
-        img: Muilas,
-        price: "Kaina 2 Eur",
-        alt: ""
-    },
+    },   
     {
         title: "Meškinų medus su bičių duonele",
         subtitle: "Medus su bičių duonele - natūralus vitaminų šaltinis iš gamtos. Sudėtis: medus, bičių duonelė. Grynas svoris 230 g. ",
@@ -39,6 +145,13 @@ export const products = [
         subtitle: "Šokoladinis medaus desertas be jokio cukraus ir dirbtinių pakaitalų. Sudėtis: medus, kakava, kakavos sviestas. Grynas svoris 230 g.",
         img: SokoladinisMedus,
         price: "Kaina 5 Eur",
+        alt: ""
+    },
+    {
+        title: "Muilas",
+        subtitle: "Šis muilas subalansuotas odos puoselėjimui, nes jo sudėtyje yra natūralaus bičių medaus bei šaltalankių aliejaus, kurie drėkina ir minkština odą. Sudėtis: natūralus bičių medus, šaltalankių aliejus, kvapusis aliejus, ožkos pienas, aukštos kokybės muilo bazė. Svoris - 50 g.",
+        img: Muilas,
+        price: "Kaina 2 Eur",
         alt: ""
     }
 ]
